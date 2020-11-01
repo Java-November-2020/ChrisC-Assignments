0fe08ff351a81c1750f381c7fd70c1b1f282703e
christophercastaneda <70987550+christophercastaneda@users.noreply.github.com>
2020-11-01 15:52:32 -0600

    Add files via upload

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,6 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        Double hypotenuse = Pythagorean.calculateHypotenuse(6, 8);
+        System.out.println(hypotenuse);
+    }
+}
