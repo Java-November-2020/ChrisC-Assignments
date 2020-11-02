1a494b68d78c0754af946e210bafbeb5069a1a1c
christophercastaneda <70987550+christophercastaneda@users.noreply.github.com>
2020-11-01 20:44:08 -0600

    Add files via upload

---
+++ 01-java-fundamentals/04-string-manipulation/StringManipulatorTest.java
@@ -0,0 +1,7 @@
+public class StringManipulatorTest{
+    public static void main(String[] args){
+       StringManipulator sm = new StringManipulator();
+
+       sm.StringManipulationTester();
+    }
+}
